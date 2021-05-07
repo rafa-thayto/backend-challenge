24ee2b165664867c20e0ded4e5b4778af3fd4f21
Rafael ThaytoTani <rafael.thaytotani@avanade.com>
2021-05-06 23:33:59 -0300

    :sparkles: feat: adding password validation rules
    Adding password validation rules, adjust response, removing Domain.csproj, adding Integration and UnitTests csprojs

--- Iti.Challenge/Iti.Challenge.Application/Commands/ValidatePassword/ValidatePasswordHandler.cs
+++ Iti.Challenge/Iti.Challenge.Application/Commands/ValidatePassword/ValidatePasswordHandler.cs
@@ -1,4 +1,6 @@
 ﻿using MediatR;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,13 +8,41 @@ namespace Iti.Challenge.Application.Commands
 {
     public class ValidatePasswordHandler : IRequestHandler<ValidatePasswordRequest, ValidatePasswordResponse>
     {
+        #region IRequestHandler implementation
+
         public Task<ValidatePasswordResponse> Handle(ValidatePasswordRequest request, CancellationToken cancellationToken)
         {
-            return Task.FromResult(
-                new ValidatePasswordResponse()
-                {
-                    Response = "Ok!"
-                });
+            var res = new ValidatePasswordResponse();
+
+            if (request.Password.Length < 9)
+            {
+                res.Message = "Sua senha precisa ter nove ou mais caracteres.";
+            }
+            else if (!(Regex.Match(request.Password, @"[A-Za-z\d]").Success))
+            {
+                res.Message = "Sua senha precisa ter letras e números.";
+            }
+            else if (!(Regex.Match(request.Password, @"[A-Z]").Success && Regex.Match(request.Password, @"[a-z]").Success))
+            {
+                res.Message = "Sua senha precisa ter pelo menos uma letra minúscula e maiúscula.";
+            }
+            else if (!Regex.Match(request.Password, @"[!,@,#,$,%,^,&,*,(,),-,+]").Success)
+            {
+                res.Message = "Sua senha precisa ter ao menos um caractere especial.";
+            }
+            else if (request.Password.ToCharArray().GroupBy(x => x).Where(y => y.Count() > 1).Select(z => z.Key).Any())
+            {
+                res.Message = "Sua senha não pode ter caracteres repetidos.";
+            } 
+            else
+            {
+                res.IsValid = true;
+                res.Message = "Senha válida!";
+            }
+
+            return Task.FromResult(res);
         }
+
+        #endregion
     }
 }
--- Iti.Challenge/Iti.Challenge.Application/Commands/ValidatePassword/ValidatePasswordResponse.cs
+++ Iti.Challenge/Iti.Challenge.Application/Commands/ValidatePassword/ValidatePasswordResponse.cs
@@ -2,6 +2,7 @@
 {
     public class ValidatePasswordResponse
     {
-        public string Response { get; set; }
+        public bool IsValid { get; set; }
+        public string Message { get; set; }
     }
 }
